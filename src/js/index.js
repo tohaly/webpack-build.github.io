4abd0580bab2b477b8cabc48e02c238929049c94
Anton Voitenko <voytenko.ad@yandex.ru>
2020-02-05 22:15:57 +0800

    Counter for left days

--- src/js/index.js
+++ src/js/index.js
@@ -19,3 +19,29 @@ goToPartners.listeners();
 document.querySelector('.header__humburger').addEventListener('click', () => {
   document.querySelector('.menu_drop-down').classList.toggle('menu_drop-down_is-opened');
 });
+
+class DateCounter {
+  constructor() {
+    this.startDate = '04/18/2020';
+    this.counter = this.counter.bind(this);
+    this.daysLeftElem = document.querySelector('.lead__days-left');
+  }
+
+  counter() {
+    const leftDays = -Math.floor((new Date() - new Date(this.startDate)) / (24 * 60 * 60 * 1000));
+    return leftDays;
+  }
+
+  render() {}
+
+  dateCheck() {
+    if (!this.counter()) {
+      this.daysLeftElem.textContent = 'Акция стартовала!';
+    }
+    this.daysLeftElem.textContent = `${this.counter()} дней`;
+  }
+}
+
+const leftDays = new DateCounter();
+
+leftDays.dateCheck();
